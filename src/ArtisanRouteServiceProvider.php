fdc12bd342b58615a1181d96e5013c6f49855d67
Abbas mkhzomi <llabbasmkhll@gmail.com>
2021-12-31 16:08:25 +0330

    service provider added and composer updated

---
+++ src/ArtisanRouteServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Llabbasmkhll\ArtisanRoute;
+
+use Illuminate\Support\ServiceProvider;
+
+class ArtisanRouteServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        //
+    }
+
+    public function boot()
+    {
+        //
+    }
+}
